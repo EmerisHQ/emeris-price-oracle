e3e8c0af76575a9eb2c3154dfa11e72e00077e3a
SpideyPool192 <krishna192reddy@gmail.com>
2021-11-17 15:31:46 +0530

    merged aggregator_test.go

--- price-oracle/database/aggregator_test.go
+++ price-oracle/database/aggregator_test.go
@@ -1,14 +1,120 @@
 package database_test
 
 import (
+	"os"
 	"testing"
 	"time"
 
+	"github.com/allinbits/emeris-price-oracle/price-oracle/daemon"
 	"github.com/allinbits/emeris-price-oracle/price-oracle/database"
 	"github.com/allinbits/emeris-price-oracle/price-oracle/types"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
+func TestMain(m *testing.M) {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	zap.ReplaceGlobals(logger)
+	os.Exit(m.Run())
+}
+
+func TestStartAggregate(t *testing.T) {
+	storeHandler, ctx, cancel, logger, cfg, tDown := setupSubscription(t)
+	defer tDown()
+	defer cancel()
+
+	// alphabetic order
+	tokens := []types.TokenPriceResponse{
+		{
+			Symbol: "ATOMUSDT",
+			Price:  10,
+		},
+		{
+			Symbol: "LUNAUSDT",
+			Price:  10,
+		},
+	}
+	stores := []string{database.BinanceStore, database.CoingeckoStore}
+	for _, token := range tokens {
+		err := storeHandler.Store.UpsertPrice(database.TokensStore, token.Price, token.Symbol, logger)
+		require.NoError(t, err)
+		for i, s := range stores {
+			err := storeHandler.Store.UpsertToken(s, token.Symbol, token.Price+float64(i+1), time.Now().Unix(), logger)
+			require.NoError(t, err)
+		}
+	}
+
+	prices, err := storeHandler.Store.GetTokens(types.SelectToken{Tokens: []string{"ATOMUSDT", "LUNAUSDT"}})
+	require.NoError(t, err)
+
+	for i, price := range prices {
+		require.Equal(t, tokens[i].Symbol, price.Symbol)
+		require.Equal(t, tokens[i].Price, price.Price)
+	}
+
+	go database.StartAggregate(storeHandler, ctx, logger, cfg, 3)
+
+	// Validate data updated on DB ..
+	require.Eventually(t, func() bool {
+		prices, err := storeHandler.Store.GetTokens(types.SelectToken{Tokens: []string{"ATOMUSDT", "LUNAUSDT"}})
+		require.NoError(t, err)
+
+		atomPrice := prices[0].Price
+		lunaPrice := prices[1].Price
+		return atomPrice == 11.5 && lunaPrice == 11.5
+
+	}, 25*time.Second, 2*time.Second)
+}
+
+func TestAggregateManager_closes(t *testing.T) {
+	storeHandler, _, cancel, logger, cfg, tDown := setupSubscription(t)
+	defer tDown()
+	defer cancel()
+
+	runAsDaemon := daemon.MakeDaemon(10*time.Second, 2, database.AggregateManager)
+	done := make(chan struct{})
+	hbCh, errCh := runAsDaemon(done, 100*time.Millisecond, logger, cfg, storeHandler.PricefiatAggregator)
+
+	// Collect 5 heartbeats and then close
+	for i := 0; i < 5; i++ {
+		<-hbCh
+	}
+	close(done)
+	_, ok := <-hbCh
+	require.Equal(t, false, ok)
+	_, ok = <-errCh
+	require.Equal(t, false, ok)
+}
+
+func TestAggregateManager_worker_restarts(t *testing.T) {
+	storeHandler, _, cancel, logger, cfg, tDown := setupSubscription(t)
+	defer tDown()
+	defer cancel()
+
+	numRecover := 2
+	runAsDaemon := daemon.MakeDaemon(10*time.Second, numRecover, database.AggregateManager)
+	done := make(chan struct{})
+	hbCh, errCh := runAsDaemon(done, 6*time.Second, logger, cfg, storeHandler.PricefiatAggregator)
+
+	// Wait for the process to start
+	<-hbCh
+	// Close the DB
+	err := storeHandler.Store.Close()
+	require.NoError(t, err)
+	// Collect 2 error logs
+	for i := 0; i < numRecover; i++ {
+		require.Contains(t, (<-errCh).Error(), "sql: database is closed")
+	}
+	// Ensure everything is closed
+	_, ok := <-errCh
+	require.Equal(t, false, ok)
+	close(done)
+	_, ok = <-hbCh
+	require.Equal(t, false, ok)
+}
 func TestPriceTokenAggregator(t *testing.T) {
 	storeHandler, _, cancel, logger, cfg, tDown := setupSubscription(t)
 	defer tDown()
--- price-oracle/store/store.go
+++ price-oracle/store/store.go
@@ -6,13 +6,14 @@ import (
 )
 
 type Store interface {
-	Init() error                                                                                      //runs migrations
+	Init() error
+	Close() error                                                                                     //runs migrations
 	GetTokens(types.SelectToken) ([]types.TokenPriceResponse, error)                                  //fetches all tokens from db tokens
 	GetFiats(types.SelectFiat) ([]types.FiatPriceResponse, error)                                     //fetches all fiat tokens from db fiats
 	GetTokenNames() ([]string, error)                                                                 //fetches whilelist with token names
 	GetPriceIDs() ([]string, error)                                                                   //fetches whilelist with price ids
 	GetPrices(from string) ([]types.Prices, error)                                                    //fetches prices from db table ex: binance,coingecko,fixer,tokens
 	UpsertPrice(to string, price float64, token string, logger *zap.SugaredLogger) error              //upsert token or fiat price in db ex: tokens, fiats
-	UpsertToken(to string, symbol string, price float64, time int64, logger *zap.SugaredLogger) error //upsert token or fiat to db. "to" indicates db name ex: binance,coingecko,fixer,tokens
-	UpsertTokenSupply(to string, symbol string, supply float64, logger *zap.SugaredLogger) error      //upsert token supply to db. "to" indicates db name ex: binance,coingecko,fixer,tokens
+	UpsertToken(to string, symbol string, price float64, time int64, logger *zap.SugaredLogger) error //upsert token or fiat to db. "to" indicates db name ex: binance,coingecko,fixer
+	UpsertTokenSupply(to string, symbol string, supply float64, logger *zap.SugaredLogger) error      //upsert token supply to db. "to" indicates db name ex: binance,coingecko,fixer
 }
