703c762b045acf44ea38f63a172b578bfe0b5234
tamjidahmed <mail.to.tamjid@gmail.com>
2022-01-17 20:54:45 +0600

    feature + test : api for coin gecko ids

---
+++ price-oracle/rest/route_getGeckoId.go
@@ -0,0 +1,39 @@
+package rest
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+const getGeckoId = "/geckoid"
+
+func (r *router) geckoIdHandler(ctx *gin.Context) {
+	namesAsString := ctx.Query("names")
+	var names []string
+	if namesAsString != "" {
+		names = strings.Split(namesAsString, ",")
+	}
+
+	// Make lowercase
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	geckoNameToId, err := r.s.sh.GetGeckoIdForToken(names)
+	if err != nil {
+		r.s.l.Errorw("Store.GetGeckoIdForToken()", "Error", err)
+		e(ctx, http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"data":    geckoNameToId,
+		"message": nil,
+	})
+}
+
+func (r *router) getGeckoId() (string, gin.HandlerFunc) {
+	return getGeckoId, r.geckoIdHandler
+}
