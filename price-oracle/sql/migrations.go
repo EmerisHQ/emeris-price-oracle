d1a43540a47972dc5d01c6d5763fbfa7fefb2731
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2022-01-18 17:50:00 +0600

    Feature: get gecko priceid (#41)
    * Get gecko id: init implementation
    
    * feature + test : gecko id
    
    * Refactor: update code quality
    
    * Shut up meg

--- price-oracle/sql/migrations.go
+++ price-oracle/sql/migrations.go
@@ -7,38 +7,39 @@ CREATE DATABASE oracle;
 `
 
 const createTableBinance = `
-CREATE TABLE oracle.binance (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
+CREATE TABLE IF NOT EXISTS oracle.binance (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
 `
 const createTableCoinmarketcap = `
-CREATE TABLE oracle.coinmarketcap (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
+CREATE TABLE IF NOT EXISTS oracle.coinmarketcap (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
 `
 const createTableCoingecko = `
-CREATE TABLE oracle.coingecko (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
+CREATE TABLE IF NOT EXISTS oracle.coingecko (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
 `
 const createTableCoinmarketcapSupply = `
-CREATE TABLE oracle.coinmarketcapsupply (symbol STRING PRIMARY KEY, supply FLOAT);
+CREATE TABLE IF NOT EXISTS oracle.coinmarketcapsupply (symbol STRING PRIMARY KEY, supply FLOAT);
 `
 const createTableCoingeckoSupply = `
-CREATE TABLE oracle.coingeckosupply (symbol STRING PRIMARY KEY, supply FLOAT);
+CREATE TABLE IF NOT EXISTS oracle.coingeckosupply (symbol STRING PRIMARY KEY, supply FLOAT);
 `
 const createTableFixer = `
-CREATE TABLE oracle.fixer (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
+CREATE TABLE IF NOT EXISTS oracle.fixer (symbol STRING PRIMARY KEY, price FLOAT, updatedat INT);
 `
 
-const createTableTokens = `CREATE TABLE oracle.tokens (symbol STRING PRIMARY KEY, price FLOAT);`
+const createTableTokens = `CREATE TABLE IF NOT EXISTS oracle.tokens (symbol STRING PRIMARY KEY, price FLOAT);`
 
-const createTableFiats = `CREATE TABLE oracle.fiats (symbol STRING PRIMARY KEY, price FLOAT);`
+const createTableFiats = `CREATE TABLE IF NOT EXISTS oracle.fiats (symbol STRING PRIMARY KEY, price FLOAT);`
+
+const createTableGeckoPriceId = `CREATE TABLE IF NOT EXISTS oracle.priceidforgecko (name VARCHAR ( 255 ) UNIQUE NOT NULL, geckoid VARCHAR ( 255 ) UNIQUE NOT NULL);`
 
 var migrationList = []string{
 	createDatabase,
 	createTableBinance,
 	createTableCoinmarketcap,
 	createTableCoinmarketcapSupply,
-	// createTableCoingecko,
-	// createTableCoingeckoSupply,
 	createTableFixer,
 	createTableTokens,
 	createTableFiats,
+	createTableGeckoPriceId,
 }
 
 var migrationCoingecko = []string{
